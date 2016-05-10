f458f1dc0e60769fedaaf93d177e908a5a46cfff
Francesco Vescio <francesco.d.vescio@gmail.com>
2016-05-10 00:39:20 -0700

    Add nice looking and functional search bar

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,11 @@ class Project < ActiveRecord::Base
     acts_as_votable
     belongs_to :user
     has_many :comments
+    def self.search(search)
+    if search
+      self.where("title LIKE ?", "%#{search}%")
+    else
+      self.all
+  	end
+  end
 end
