4d180915654fba85dc6e0c192990f526cb8dc7ba
flyksky <flyksky@gmail.com>
2016-05-12 05:34:11 +0000

    Add migration for name field; enale user to edit name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512031553) do
+ActiveRecord::Schema.define(version: 20160512051711) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "commentable_id"
@@ -116,7 +116,6 @@ ActiveRecord::Schema.define(version: 20160512031553) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
-    t.string   "name",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -128,6 +127,7 @@ ActiveRecord::Schema.define(version: 20160512031553) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
