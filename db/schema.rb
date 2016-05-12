5a35144e5f1c74d72643b6dd7a32cf00f5cfb748
Calvin <nivlac123@gmail.com>
2016-05-11 21:49:34 -0700

    More merge issues fixed v2

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20160512031553) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
+    t.string   "name",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
